25211c81a1fbc1228d6558ad41e6c83e5a9cac5c
Jack Barrera <49659133+jbarrera30@users.noreply.github.com>
2020-07-05 00:05:56 -0700

    flexibility changes

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from functions import *
 
 def main():
     url = '<url here>'
-    links = []
     original_html = initialize_listing(url)
     while True:
         new_html = initialize_listing(url)
         if original_html != new_html:
             original_posts = original_html.find_all('li', class_='result-row')
             new_posts = new_html.find_all('li', class_='result-row')
+            links = []
             for item in new_posts:
                 if item not in original_posts:
                     link = item.find('a', class_='result-title hdrlnk')
